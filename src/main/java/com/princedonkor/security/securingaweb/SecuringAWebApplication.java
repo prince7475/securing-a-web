108151239e35f41ecbca64d031ffbcbeffe9c30a
Prince Donkor <prince7475>
2019-01-01 15:53:13 -0500

    First push

---
+++ src/main/java/com/princedonkor/security/securingaweb/SecuringAWebApplication.java
@@ -0,0 +1,14 @@
+package com.princedonkor.security.securingaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringAWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuringAWebApplication.class, args);
+	}
+
+}
+
